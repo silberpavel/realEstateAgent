a6fb2eacac766b08a7f3dc7b218f5a19a8918833
silberpavel <silber.ypm@gmail.com>
2018-03-05 10:19:17 +0200

    page preloader

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+
+    $(".dws-progress-bar").circularProgress({
+      color: "#25ffe4",
+      line_width: 17,
+      height: "350px",
+      width: "350px",
+      percent: 0,
+      // counter_clockwise: true,
+      starting_position: 25
+  }).circularProgress('animate', 100, 2000);
+
     var videobackground = new $.backgroundVideo($('body'), {
       "align": "centerXY",
       "width": 1280,
@@ -10,4 +21,10 @@ $(document).ready(function() {
       "autoplay": true,
       "loop": true
     });
-  });
+  
+});
+
+$(window).on('load', function () {
+  var $preloader = $('#preloader');
+  $preloader.delay(1800).fadeOut('slow');
+});
